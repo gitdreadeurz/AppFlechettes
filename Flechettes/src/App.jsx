7ddc1af6c065157c63242f65aa1a3ab1f1d01c8d
Nicolas Pollet <you@example.com>
2026-03-23 12:13:00 +0100

    Nico

---
+++ Flechettes/src/App.jsx
@@ -0,0 +1,13 @@
+import GamePage from "./pages/GamePage";
+import GamePageia from "./pages/GamePageIA";
+
+function App () {
+
+    return <>
+    <GamePage/>
+    
+    
+    </>;
+}
+ 
+export default App;
